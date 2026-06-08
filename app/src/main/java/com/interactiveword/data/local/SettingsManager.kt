f7a9f18f34d1677b451e803cb14d3f9b4bfed709
hamseungyeal <ham.seungyeal@gmail.com>
2026-06-08 18:36:31 +0900

    feat: IP

--- app/src/main/java/com/interactiveword/data/local/SettingsManager.kt
+++ app/src/main/java/com/interactiveword/data/local/SettingsManager.kt
@@ -6,7 +6,7 @@ import androidx.core.content.edit
 object SettingsManager {
     private const val PREFS_NAME = "settings_prefs"
     private const val KEY_BASE_URL = "base_url"
-    const val DEFAULT_BASE_URL = "http://13.209.74.220:8000/"
+    const val DEFAULT_BASE_URL = "http://43.201.22.217:8000/"
 
     fun saveBaseUrl(context: Context, url: String) {
         context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
